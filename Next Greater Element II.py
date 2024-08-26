67cc117d7b2579156c613b42d0d754dfeb505e91
NikithaLingineni <113195326+NikithaLingineni@users.noreply.github.com>
2024-08-26 11:07:42 +0530

    Stack and Queue

---
+++ Next Greater Element II.py
@@ -0,0 +1,15 @@
+def nextgreater(nums):
+    n=len(nums)
+    nge=[-1]*n
+    st=[]
+    for i in range(2*n-1,-1,-1):
+        while st and st[-1]<=nums[i%n]:
+            st.pop()
+        if i<n:
+            if st:
+                nge[i]=st[-1]
+        st.append(nums[i%n])
+    return nge
+nums=list(map(int,input().split()))
+res=nextgreater(nums)
+print(res)
